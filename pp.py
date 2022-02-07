4d3045fd66a669748044a18db7e604009d46576a
Jonathan Harris <jih1@protonmail.com>
2022-02-07 18:54:15 +1300

    Now generating random sequence

--- pp.py
+++ pp.py
@@ -1,10 +1,19 @@
+import os
+import random
 import time
 import pyttsx3
 import numpy as np
 import wave
 from scipy.io.wavfile import write
 
+NUM_NOTES_IN_RANDOM_SEQ=500
+
+DURATION=10.0 # seconds
+FIRST_OCTAVE=3
+LAST_OCTAVE=5
+
 all_notes=["C", "C sharp", "D", "E flat", "E", "F", "F sharp", "G", "A flat", "A", "B flat", "B" ]
+generate_notes=["C", "D", "E", "F", "G", "A", "B" ]
 
 A4=440
 A0=A4/(2**4)
@@ -17,15 +26,19 @@ def get_note_frequency(note,number):
 
     return freq0*(2**number)
 
+def get_filename(note,number):
+    return f'{note} {number}.wav'.replace(" ","-")
+
 def generate_tone_file(note,number):
+    if os.path.exists(get_filename(note,number)):
+        return
 # generate the tone
     volume = 1.0
     fs = 22050
-    duration = 5.0
     f = get_note_frequency(note,number)
     print(f)
 
-    samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
+    samples = (np.sin(2*np.pi*np.arange(fs*DURATION)*f/fs)).astype(np.float32)
     scaled = np.int16(samples/np.max(np.abs(samples)) * 2**15)
     write('tone.wav',fs,scaled)
 
@@ -38,7 +51,7 @@ def generate_tone_file(note,number):
 
 # concatenate the tone and the note name
     infiles = ["tone.wav", "note.wav"]
-    outfile = note_name.replace(' ','-')+".wav"
+    outfile = get_filename(note,number)
 
     data=[]
 
@@ -54,6 +67,26 @@ def generate_tone_file(note,number):
         output.writeframes(data[i][1])
     output.close
 
+
+
+
+# MAIN
+
+# generate all of the note files
 for note in all_notes:
-    for octave in range(1,8):
+    for octave in range(FIRST_OCTAVE,LAST_OCTAVE+1):
         generate_tone_file(note, octave)
+
+sequence=[]
+for i in range(NUM_NOTES_IN_RANDOM_SEQ):
+    note=random.choice(generate_notes)
+    octave=random.randrange(FIRST_OCTAVE,LAST_OCTAVE+1)
+    w = wave.open(get_filename(note,octave),'rb')
+    sequence.append( [w.getparams(), w.readframes(w.getnframes())] )
+    w.close()
+
+output = wave.open("sequence.wav",'wb')
+output.setparams(sequence[0][0])
+for i in range(len(sequence)):
+    output.writeframes(sequence[i][1])
+output.close
